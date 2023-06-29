756b75a307f1dfeb624e80be095bf6b7577d6c98
Asgarov Asgar <asgara@maestro.st>
2023-06-29 23:57:49 +0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+import AuthModal from './components/AuthModal.vue'
+
+const searchUsername = ref('')
+
+const onSearch = () => {
+  console.log('searhcing', searchUsername.value);
+}
+
+</script>
+
+<template>
+  <header class="bg-gray-900 flex justify-center">
+    <div class="p-4 container flex justify-between">
+
+      <div class="flex gap-4 items-center">
+        <RouterLink to="/" class="font-bold text-2xl text-white">Instagram</RouterLink>
+        <input v-model="searchUsername" class="p-1.5" type="text" placeholder="username..." @keypress.enter="onSearch">
+      </div>
+      <div class="text-white">
+        <button class="px-4 py-1.5 bg-blue-500">Sign up</button>
+        <AuthModal />
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/components/AuthModal.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+const visible = ref(false)
+const openModal = () => visible.value = true
+</script>
+
+<template>
+  <button class="px-4 py-1.5 bg-blue-500" @click="openModal">Login</button>
+  <div v-if="visible" class="fixed top-0 left-0 w-full h-full z-10 bg-gray-900/70 text-black flex justify-center items-baseline pt-20">
+    <div class="w-full max-w-xl bg-white p-6 rounded-lg shadow-lg">
+      <div>
+        asds
+      </div>
+      <div class="border-t px-4 pt-4 flex gap-4 justify-end">
+        <button class="px-4 py-1.5 border rounded">Cancel</button>
+        <button class="px-4 py-1.5 border rounded bg-blue-500 text-white">OK</button>
+      </div>
+    </div> 
+  </div>
+</template>
